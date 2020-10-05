bc7dc8d6215eca59946ab881f3e941adb7d0b7ba
Crew Spence <csspence123@gmail.com>
2020-10-05 07:32:53 -0500

    Solved toy problem.

--- udemy_DLL_33_insert_remove.js
+++ udemy_DLL_33_insert_remove.js
@@ -10,7 +10,6 @@ This internal/helper function should insert a node at a specified index in a Dou
 remove
 This function should remove a node at a specified index in a DoublyLinkedList.  It should return the removed node if the index is valid, or undefined if the index is invalid.
 */
-
 class Node {
     constructor(val){
         this.val = val;
@@ -40,10 +39,66 @@ class DoublyLinkedList {
          }
         return this;
     }
-    insert(){
-        
+    insert(index, val){
+      let node;
+      let counter;
+      let newNode = new Node(val);
+      if(index = 0) {
+        this.head.prev = newNode;
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return true;
+      }
+      if(index < 0 || index > this.length) {
+        return false;
+      } else if(index < this.length / 2) {
+        node = this.head;
+        counter = 0;
+        while(counter !== index) {
+          node = node.next;
+          counter++;
+        }
+      } else {
+        node = this.tail;
+        counter = this.length - 1;
+        while(counter !== index) {
+          node = node.prev;
+          counter--;
+        }
+      }
+      node.prev.next = newNode;
+      newNoxe.prev = node.prev;
+      newNode.next = node;
+      node.prev = newNode;
+      this.length++;
+      return true;
     }
-    remove(){
-        
+    remove(index){
+      let node;
+      let counter;
+      if(index < 0 || index > this.length) {
+        return undefined;
+      } else if(index < this.length / 2) {
+        node = this.head;
+        counter = 0;
+        while(counter !== index) {
+          node = node.next;
+          counter++;
+        }
+      } else {
+        node = this.tail;
+        counter = this.length - 1;
+        while(counter !== index) {
+          node = node.prev;
+          counter--;
+        }
+      }
+      node.prev.next = node.next;
+      node.next.prev = node.prev;
+      node.prev = null;
+      node.next = null;
+      this.length--;
+      return node;
     }
 }
